2a6e679dbad6b8931bdb2dfb37cddbbcfc05ad48
dboyd42 <bhatmagician@gmail.com>
2020-01-17 15:10:30 -0600

    [+] scriptPATH, git clone VundleVim

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -3,28 +3,25 @@
 # Program: Link Dotfiles
 # Description:
 #     Soft links all the dotfiles.
+#     Installs Vundle Vim pkg mgr.
 # Date: 2020-01-17
 # Revised:
-#     <revision date>
+#    2020-01-17 - [+]script-path, [+] git Vundle
 
-## Determine origin of script execution
-#scriptPath="/dotfiles"
-## Make sure your $PWD=../dotfiles
-#if [ "$PWD" != "*/${scriptPath}" ]
-#then
-#    echo "This script needs to be executed in the $scriptPath"
-#    exit
-#fi
+# Determine origin of script execution
+if [[ "$PWD" != *dotfiles ]]
+then
+    echo "This script needs to be executed in the 'dotfiles' directory."
+    exit
+fi
 
 # Determine OS
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     os="l"
-                usrVimPath="/usr/share/vim/*"
+    Linux*)     usrVimPath="/usr/share/vim/*"
                 bashAliases="$PWD/bash/.bash_aliases"
                 ln -sf $bashAliases ~/.bash_aliases;;
-    Darwin*)    os="m"
-                usrVimPath="/usr/local/share/vim/*"
+    Darwin*)    usrVimPath="/usr/local/share/vim/*"
                 bashProfile="bash/macOS/.bash_profile"
                 ln -sf $bashProfile ~/.bash_profile;;
 
@@ -32,18 +29,20 @@ case "${unameOut}" in
                 exit
 esac
 
-# Linking Vim dotfiles
+# Link Vim dotfiles
+echo "linking dotifles..."
 ln -sf $PWD/vim/.vimrc ~/.vimrc
 ln -sf $PWD/vim/* $usrVimPath
 # Install plugins
-# +May cause problems --haven't tested on new machine!
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "cloning VundleVim..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+echo
 
-# Verify links
+# Verify and Display links
 echo "Checking links"
 echo "=============="
-if [ $os == "l" ]
+if [ $unameOut == "Linux" ]
 then
     ls -al ~/.bash_aliases
 else

c53058be597bd5adcae7d3776d837c8b0b50d61b
dboyd42 <bhatmagician@gmail.com>
2020-04-01 17:10:57 -0700

    added prgms

--- windows/aliases.bat
+++ windows/aliases.bat
@@ -1,9 +1,4 @@
 @ECHO OFF
-:: site: https://stackoverflow.com/questions/20530996/aliases-in-windows-command-prompt
-:: Make Persistent
-:: 1) regedit -> HKEY_CURRENT_USER\Software\Microsoft\Command Processor
-:: 2) add 'String Value' entry w/ name 'AutoRun' and the FULL/path/to/aliases.cmd
-:: NOTES: Run multi cmds: parallel '&'; after 1st cmd '&&'
 
 :: Commands
 :: ========
@@ -12,8 +7,8 @@
 DOSKEY ga=git add
 DOSKEY gaR=git add README.rst; git commit -m "updated"; git push
 DOSKEY gas=git add . && git status
-:: DOSKEY gc=git commit -m  :: asks for a "value"
 :: git commit requires double quotes!
+DOSKEY gc=git commit -m
 DOSKEY gcip=git commit -m "initial commit"; git push
 DOSKEY gco=git checkout -b
 DOSKEY gcup=git commit -m "updated" && git push
@@ -23,6 +18,10 @@ DOSKEY gl=git pull
 DOSKEY gp=git push
 DOSKEY gr=git reset
 DOSKEY gst=git status
+
+:: programs
+DOSKEY com=g++ -o main *.cpp
+DOSKEY py=python3
 
 ::vim
 DOSKEY v=vim

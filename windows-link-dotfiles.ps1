8ba9cccd48828b097c2ab8682dc40aa3093040b5
bhatmagick <bhatmagician@gmail.com>
2020-09-14 10:22:52 -0500

    added meta-comments

--- windows-link-dotfiles.ps1
+++ windows-link-dotfiles.ps1
@@ -1,21 +1,35 @@
-# Admin privileges required
-#Start-Process Powershell.exe -Verb runAs
+# Copyright 2020 David Boyd, all rights reserved
+# Program: Windows Link Dotfiles
+# Description:
+#     This script automates the configuration setup for Vim's dotfiles
+#     including plug-ins.
+# Note: Admin privileges required for 'SymbolicLink'
+# Date: 2020-09-13
+# Revised:
+
 
 ###
 ### Link dotfiles
 ###
+# Link dotfiles\vim\* to C:\tools\vim\
+# Link dotfiles\vim\.vimrc to C:\tools\_vimrc
 
-# $VIMRUNTIMEPATH == C:\tools\vim\vim82\
+# Dir: abbrev
 $myTarget = $Env:Userprofile + '\Documents\code\repos\github\dboyd42\dotfiles\vim\abbrev'
 $myPath = 'C:\tools\vim\abbrev'
 New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget
+
+# Dir: src
 $myTarget = $Env:Userprofile + '\Documents\code\repos\github\dboyd42\dotfiles\vim\src'
 $myPath = 'C:\tools\vim\src'
 New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget
+
+# Dir: templates
 $myTarget = $Env:Userprofile + '\Documents\code\repos\github\dboyd42\dotfiles\vim\templates'
 $myPath = 'C:\tools\vim\templates'
 New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget
-# Link $MYVIMRC == C:\tools\_vimrc
+
+# File: vimrc
 $myTarget = $Env:Userprofile + '\Documents\code\repos\github\dboyd42\dotfiles\vim\.vimrc'
 $myPath = 'C:\tools\vim\_vimrc'
 New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget
@@ -27,7 +41,7 @@ New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget
 # Create path for plug-ins: vim82\pack\plug-ins\start   //start is used for ln
 New-Item -Path 'C:\tools\vim\vim82\pack\plug-ins\' -ItemType Directory
 
-# Link git submodules to /start folder
+# Link git plug-in submodules to /start folder
 $myTarget = $Env:Userprofile + '\Documents\code\repos\github\dboyd42\dotfiles\pack\plug-ins\start'
 $myPath = 'C:\tools\vim\vim82\pack\plug-ins\start'
 New-Item -ItemType SymbolicLink -Path $myPath -Target $myTarget

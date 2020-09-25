0ba4b7a1ebbc87994d5ec6599b929b9ffecf92fe
dboyd42 <bhatmagician@gmail.com>
2020-09-24 23:17:55 -0500

    resolved Ubuntu@WSL: diff(vim -version) issues

--- linux-link-dotfiles.sh
+++ linux-link-dotfiles.sh
@@ -6,37 +6,37 @@
 #   plug-ins), bash_aliases,.tmux.conf, and gitconfig.
 # Date: 2020-01-17
 # Revised:
-#    2020-09-24 - updating $VIMRUNTIME = vim82;
-#       [BUG]37 - Ubuntu::WSL uses vim81
-#       [BUG]63 - Kali::WSL permission denied; Enter manually
-#    2020-09-22 - revamped to match windows-link-dotfiles
-#    2020-02-06 - added gitconfig to Linux
-#    2020-01-17 - [+]script-path, [+] git Vundle
+#	2020-09-24 - updating $VimRunTime = vim82;
+#	   [BUG]63 - Kali::WSL permission denied; Enter manually
+#	   [Resolved] [BUG]37 - Ubuntu::WSL uses vim81
+#	2020-09-22 - revamped to match windows-link-dotfiles
+#	2020-02-06 - added gitconfig to Linux
+#	2020-01-17 - [+]script-path, [+] git Vundle
 
 # Determine origin of script execution
 if [[ "$PWD" != *dotfiles ]]
 then
-    echo "This script needs to be executed in the 'dotfiles' directory."
-    exit
+	echo "This script needs to be executed in the 'dotfiles' directory."
+	exit
 fi
 
 ###
 ### Link dotfiles
 ###
-# $MYVIMRC    = /home/<user>
-# $VIM        = /usr/share/vim/
-VIM="/usr/share/vim"
+# $MYVimRC	= /home/<user>
+# $Vim		= /usr/share/vim/
+Vim="/usr/share/vim"
+# Ubuntu@WSL uses version Vim81; not Vim82
+wsl_ubuntu=$(ls /etc/ | grep ubuntu | cut -d '-' -f1)
 
-# [BUG] Ubuntu::WSL uses vim81
-kernel_release=$(uname -r | cut -d '-' -f2) # WSL == microsoft
-if [ $kernel_release = "kali1" ] || [ $kernel_release = "microsoft" ]; then
-	# Kali      : $VIMRUNTIME = /usr/share/vim/vim82/
-	VIMRUNTIME="/usr/share/vim/vim82"
+if [ $wsl_ubuntu = "ubuntu" ]; then
+	# Ubuntu@WSL: $VimRunTime = /usr/share/vim/vim81/
+	VimRunTime="/usr/share/vim/vim81/"
 else
-	# Ubuntu@WSL: $VIMRUNTIME = /usr/share/vim/vim81/
-	VIMRUNTIME="/usr/share/vim/vim81/"
+	# Kali      : $VimRunTime = /usr/share/vim/vim82/
+	# Kali@WSL  : $VimRunTime = /usr/share/vim/vim82/
+	VimRunTime="/usr/share/vim/vim82"
 fi
-# [BUG] end
 
 # Link dotfiles/vim/* to
 # Link dotfiles/vim/.vimrc to $HOME/.vimrc
@@ -44,24 +44,24 @@ fi
 # Link dotfiles/bash/.tmux.conf to $HOME/.tmux.conf
 # Link dotfiles/bash/.bash_aliases to $HOME/.bash_aliases
 
-# Dir: abbrev, src, templates           # :echo $VIM
-ln -sf $PWD/vim/abbrev/ $VIM/
-ln -sf $PWD/vim/src/ $VIM/
-ln -sf $PWD/vim/templates/ $VIM/
+# Dir: abbrev, src, templates		   # :echo $VIM
+ln -sf $PWD/vim/abbrev $Vim/
+ln -sf $PWD/vim/src $Vim/
+ln -sf $PWD/vim/templates $Vim/
 
-# File: vimrc                           # :echo $MYVIMRC
+# File: vimrc						   # :echo $MYVIMRC
 ln -sf $PWD/vim/.vimrc $HOME/.vimrc
 
-# File: .gitconfig                      # git config --list --show-origin
+# File: .gitconfig					   # git config --list --show-origin
 ln -sf $PWD/gitconfig $HOME/.gitconfig
 
 # File: .tmux.conf
 ln -sf $PWD/bash/.tmux.conf $HOME/.tmux.conf
 
+# [BUG] Kali@WSL permission denied; Enter manually ############################
 # File: .bash_aliases
-# [BUG] Kali::WSL permission denied; Enter manually
 ln -sf $PWD/bash/.bash_aliases $HOME/.bash_aliases
-# [BUG] end
+# [BUG] end ###################################################################
 
 ###
 ### Plug-ins Installation
@@ -69,11 +69,11 @@ ln -sf $PWD/bash/.bash_aliases $HOME/.bash_aliases
 # Clone plug-in submodules after cloning dotfiles
 git submodule update --init
 
-# Create path for plug-ins: vim81/pack/plug-ins/start
-mkdir -p $VIMRUNTIME/pack/plug-ins/start/
+# Create path for plug-ins: vim8[12]/pack/plug-ins/start
+mkdir -p $VimRunTime/pack/plug-ins
 
 # Link git plug-in submodules to /start folder
-ln -sf $PWD/pack/plug-ins/start/ $VIMRUNTIME/pack/plug-ins/start/
+ln -sf $PWD/pack/plug-ins/start $VimRunTime/pack/plug-ins/start
 
 ###
 ### Enable Aliases & Reset Terminal
@@ -82,5 +82,5 @@ ln -sf $PWD/pack/plug-ins/start/ $VIMRUNTIME/pack/plug-ins/start/
 source ~/.bashrc
 
 # Reset terminal
-#reset
+reset
 

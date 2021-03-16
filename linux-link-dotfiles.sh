162fdd37dacdd710d28b6c9b8c960e03d75665fe
dboyd42 <bhatmagician@gmail.com>
2021-03-15 20:45:32 -0500

    bash->zsh configurations

--- linux-link-dotfiles.sh
+++ linux-link-dotfiles.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 #!/bin/bash
 # Copyright 2019 David Boyd, all rights reserved
 # Program: Link Dotfiles
@@ -5,7 +6,13 @@
 #   This script automates the configuration setup for Vim's dotfiles (and
 #   plug-ins), bash_aliases,.tmux.conf, and gitconfig.
 # Date: 2020-01-17
+# Nots: To properly link files, run the script as both sudo, then as a regular user.
 # Revised:
+#       2021-03-13 - zsh
+#           [+] 01: ZSH shebang 						// macOS, Kali 20.3
+#           [+] 02: [manual entry] $ sudo mkdir -p $VimRunTime/pack/plug-ins	// macOS
+#           [+] 03: [manual entry] $ sudo ln -sf $PWD/pack/plug-ins/start $VimRunTime/pack/plug-ins/  // macOS
+#           [+] 04: concat alias >> .zshrc && source .zshrc
 #	2020-09-24 - updating $VimRunTime = vim82;
 #	   [BUG]63 - Kali::WSL permission denied; Enter manually
 #	   [Resolved] [BUG]37 - Ubuntu::WSL uses vim81
@@ -25,21 +32,27 @@ fi
 ###
 # $MYVimRC	= /home/<user>
 # $Vim		= /usr/share/vim/
-Vim="/usr/share/vim"
 
+# Determine OS
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	Darwin*) Vim="/usr/local/share/vim";;
+	Linux*)  Vim="/usr/share/vim"
+		 VimRunTime="/usr/share/vim/vim82"
+	         ### Ubuntu@WSL uses version Vim81; Kali@WSL uses version Vim82
+		 ### read -p DOES NOT WORK with ZASH!!! Only with Bash!
+		 #read -p 'Which WSL are you using? [k/u/q]: ' wsl_type
 
-### Ubuntu@WSL uses version Vim81; Kali@WSL uses version Vim82
-read -p 'Which WSL are you using? [k/u/q]: ' wsl_type
-
-if [ $wsl_type = "u" ]; then
-	# Ubuntu@WSL: $VimRunTime = /usr/share/vim/vim81/
-	VimRunTime="/usr/share/vim/vim81/"
-elif [ $wsl_type = "k" ]; then
-	# Kali@WSL  : $VimRunTime = /usr/share/vim/vim82/
-	VimRunTime="/usr/share/vim/vim82"
-else
-	exit
-fi
+		 #if [ $wsl_type = "u" ]; then
+		 #	# Ubuntu@WSL: $VimRunTime = /usr/share/vim/vim81/
+		 #	VimRunTime="/usr/share/vim/vim81/"
+		 #elif [ $wsl_type = "k" ]; then
+		 #	# Kali@WSL  : $VimRunTime = /usr/share/vim/vim82/
+		 #	VimRunTime="/usr/share/vim/vim82"
+		 #fi
+		 ;;
+	*) echo "UNKNOWN:${unameOut}";;
+esac
 
 ### Link Overview ###
 # Link dotfiles/vim/* to
@@ -82,9 +95,12 @@ ln -sf $PWD/pack/plug-ins/start $VimRunTime/pack/plug-ins/
 ###
 # Enable .bash_aliases through .bashrc
 #[BUG] Does NOT source bashrc file
-echo "source ~/.bash_aliases" >> ~/.bashrc
-source ~/.bashrc
-echo "If .bash_aliases are not working, manually source your bashrc file."
+#echo "source ~/.bash_aliases" >> ~/.bashrc
+#source ~/.bashrc
+#echo "If .bash_aliases are not working, manually source your bashrc file."
+echo "source ~/.bash_aliases" >> ~/.zshrc
+source ~/.zshrc
+echo "If .bash_aliases are not working, manually source your zsh file."
 
 # Reset terminal
 #reset

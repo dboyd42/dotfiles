8af2596849c9534508323911c538d59dd570229a
dboyd42 <bhatmagician@gmail.com>
2021-04-24 13:59:12 -0500

    init commit

---
+++ p10k-omz-install.zsh
@@ -0,0 +1,14 @@
+
+
+### If error 'zsh: command not found: p10k' occurs:
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+### *Nix Fonts: (MesloLGS NF Regular.tff)
+# https://github.com/romkatv/powerlevel10k/blob/master/font.md
+# Linux install:
+#mkdir ~/.local/share/fonts
+#mv <fontFile> ~/.local/share/fonts
+#fc-cache -f -v
+#fc-list | grep <fontName>
+

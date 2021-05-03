7b18fc54a4d29c5e52cc101a9ad8adfeb2a7a044
dboyd42 <bhatmagician@gmail.com>
2021-05-02 21:03:05 -0500

    [+] sed zshrc > THEME, plugins

--- scripts/linux/wsl-fresh-install-part-01.sh
+++ scripts/linux/wsl-fresh-install-part-01.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Copyright 2021 David Boyd, all rights reserved
 
 # Install preliminary programs
 sudo apt-get install vim-gtk3 -y
@@ -14,22 +16,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Powerlevel10k setup
 reset
 
-# Install required fonts for Powerlevel10k theme 
+# Install required fonts for Powerlevel10k theme
 #   --check win-terminal: MesloLGS NF
-
-# Install Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Install ZSH plugins
-#   zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-#   zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
-# Update the theme name inside .zshrc file
-echo '----------------------------------------'
-echo 'Update the theme name inside .zshrc file'
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"'
-echo ' THEN restart your terminal....
-echo '----------------------------------------'
 
--- scripts/linux/wsl-fresh-install-part-02.sh
+++ scripts/linux/wsl-fresh-install-part-02.sh
@@ -1,4 +1,7 @@
-# Install required fonts for Powerlevel10k theme 
+#!/bin/bash
+# Copyright 2021 David Boyd, all rights reserved
+
+# Install required fonts for Powerlevel10k theme
 #   --check win-terminal: MesloLGS NF
 
 # Install Powerlevel10k
@@ -11,13 +14,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Update the theme name inside .zshrc file
+sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/' ~/.zshrc
+# Update the plugins inside .zshrc file
+sed -i 's/=(git)/=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
 echo '----------------------------------------'
-echo 'Update the theme name inside .zshrc file'
-echo '> ZSH_THEME="powerlevel10k/powerlevel10k" '
-echo '-- -- -- -- -- -- -- -- -- -- -- -- -- -- '
-echo 'Update the plugin=(git) inside .zshrc file'
-echo '> plugins=(git zsh-autosuggestions zsh-syntax-highlighting)'
-echo '-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --'
-echo ' THEN restart your terminal....         '
+echo ' RESTART your terminal.... (not reset!) '
+echo '----------------------------------------'
+echo 'IF font is jacked up, refer->MesloLGS NF'
 echo '----------------------------------------'
 

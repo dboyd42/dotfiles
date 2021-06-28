56b4d24a9981f5bc1ad684084da709e5b069ecf2
dboyd42 <bhatmagician@gmail.com>
2021-06-28 12:46:04 -0500

    fixed echo output, debug: powerlevel10k

--- experimental-install.linux.zsh
+++ experimental-install.linux.zsh
@@ -2,6 +2,13 @@
 ### ZSH with Antigen
 ###     > autojump, colored-man-pages, ccat
 ###-----------------------------------------###
+### ERRORS:
+###     > hangs on antigen bundle powerlevel10k
+###-----------------------------------------###
+### Updates:
+###     > create .antigenrc file
+###-----------------------------------------###
+
 
 # Install dependencies
 sudo apt install curl -y
@@ -17,30 +24,34 @@ curl -L git.io/antigen > ~/antigen.zsh
 echo ". ~/antigen.zsh" >> ~/.zshrc
 echo "antigen init ~/.antigenrc" >> ~/.zshrc
 # Populate antigenrc file
-echo -e "# Load the oh-my-zsh's library.\n
-antigen use oh-my-zsh\n
-\n
-# Bundles from the default repo (robbyrussell's oh-my-zsh).\n
-antigen bundle git\n
-antigen bundle heroku\n
-antigen bundle pip\n
-antigen bundle lein\n
-antigen bundle command-not-found\n
-antigen bundle docker\n
-antigen bundle autojump\n
-antigen bundle ael-code/zsh-colored-man-pages\n
-antigen bundle hcgraf/zsh-sudo\n    # <Esc-Esc> curr->line ::> sudo curr->line
-\n
-# Syntax highlighting bundle.\n
-antigen bundle zsh-users/zsh-syntax-highlighting\n
-antigen bundle zsh-users/zsh-autosuggestions\n
-antigen bundle zsh-users/zsh-completions\n
-\n
-# Load the theme.\n
-#antigen theme robbyrussell     # default\n
-antigen theme romkatv/powerlevel10k\n
-\n
-# Tell Antigen that you're done.\n
-antigen apply\n" >> ~/.antigenrc
+echo -e "# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
 
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+antigen bundle docker
+antigen bundle autojump
+antigen bundle ael-code/zsh-colored-man-pages
+antigen bundle hcgraf/zsh-sudo    # <Esc-Esc> curr->line ::> sudo curr->line
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+
+# Load the theme.
+#antigen theme robbyrussell     # default
+antigen theme romkatv/powerlevel10k
+
+# Tell Antigen that you're done.
+antigen apply" >> ~/.antigenrc
+
+echo
+echo
 echo "To configure p10k theme: $ p10k configure"
+echo "Exit; then Restart your shell"
+

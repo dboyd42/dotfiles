c633b63c71002ac143ad22c1557c498f0d54f1f7
dboyd42 <bhatmagician@gmail.com>
2023-01-05 14:19:53 -0600

    add vim-plug, zsh-autosuggestions & syntax-highlighting

--- install-arco.sh
+++ install-arco.sh
@@ -1,8 +1,16 @@
 ########## DEPENDENCIES ########## 
 sudo pacman -S xclip neovim tmux --noconfirm
 
+### ZSH/OH-MY-ZSH ###
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 ##### NEOVIM ##### 
-sudo pacman -S node npm ruby python perl-cpanplus-dist-arch --noconfirm
+# LSP
+sudo pacman -S node npm ruby --noconfirm
+# Vim-Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Python
 pip install pynvim
@@ -15,20 +23,27 @@ gem environment
 # Nodejs & Npm
 npm install -g neovim
 
-# Perl #TODO --not working; ::disable
-# let g:loaded_perl_provider=0
-
 ########## LINKING  ########## 
-ln -sf $PWD/home/.tmux.conf /home/$USER/.tmux.conf; \
-ln -sf $PWD/home/.gitconfig /home/$USER/.gitconfig; \
-ln -sf $PWD/home/.zsh_aliases /home/$USER/.zsh_aliases; \
+ln -sf $PWD/home/.tmux.conf    $HOME/.tmux.conf; \
+ln -sf $PWD/home/.gitconfig    $HOME/.gitconfig; \
+ln -sf $PWD/home/.zsh_aliases  $HOME/.zsh_aliases; \
 sudo ln -sf $PWD/etc/logid.cfg /etc/logid.cfg
+
+### NVIM ###
+mkdir -p $HOME/.config/nvim/vimfiles; \
+ln -sf $PWD/home/.config/nvim/init.vim              $HOME/.config/nvim/init.vim; \
+ln -sf $PWD/home/.config/nvim/vimfiles/au.vim       $HOME/.config/nvim/vimfiles/au.vim; \
+ln -sf $PWD/home/.config/nvim/vimfiles/mappings.vim $HOME/.config/nvim/vimfiles/mappings.vim; \
+ln -sf $PWD/home/.config/nvim/vimfiles/settings.vim $HOME/.config/nvim/vimfiles/settings.vim; \
+ln -sf $PWD/home/.config/nvim/vimfiles/plugins.vim  $HOME/.config/nvim/vimfiles/plugins.vim
+
+nvim +PlugInstall +qa!
+
 
 ###
 ### TODO
 ###
 # [WORKING]
-# Ruby PATH issue
-# addto init.vim: 
-# get path via: sudo find / -iname "neovime-ruby-host" 2>/dev/null
+# get path via: sudo find / -iname "neovim-ruby-host" 2>/dev/null
 # let g:ruby_host_prog = '/home/$USER/.local/share/gem/ruby/3.0.0/bin/neovim-ruby-host'
+# let g:loaded_perl_provider=0

3a39e43e77bf12f01873bf5042a1f12ca2ad3bb3
dboyd42 <bhatmagician@gmail.com>
2023-01-07 22:14:14 -0600

    [+] comment.nvim

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,18 +4,19 @@ vim.g.ruby_host_prog = '/home/bhatm/.local/share/gem/ruby/3.0.0/bin/neovim-ruby-
 vim.g.loaded_perl_provider = 0
 
 ---- Load local modules -------------------------------------------------------
-require('barbar-config')               -- tabline
+require('barbar-config')                -- tabline
 require('colorschemes-config.dracula')
-require('mason-config') 		       -- LSP mgr
-require('lsp-config.language-servers') -- LSP servers
-require('lsp-config.nvim-cmp')         -- autocompletion
-require('lualine-config')              -- statusline
-require('mappings') 		           -- key-bindings
-require('nvim-autopairs-config')	   -- autopairs
-require('nvim-notify-config')	       -- notifications
-require('nvim-tree-config')	           -- file-explorer
-require('packer-config')	           -- plugin mgr
-require('settings')                     
+require('comment-config')               -- Commenter
+require('mason-config') 		        -- LSP mgr
+require('lsp-config.language-servers')  -- LSP servers
+require('lsp-config.nvim-cmp')          -- autocompletion
+require('lualine-config')               -- statusline
+require('mappings') 		            -- key-bindings
+require('nvim-autopairs-config')	    -- autopairs
+require('nvim-notify-config')	        -- notifications
+require('nvim-tree-config')	            -- file-explorer
+require('packer-config')	            -- plugin mgr
+require('settings')                     -- personal settings
 --require('shade-config')                -- window dimmer
-require('treesitter-config')           -- syntax hl & language parser
+require('treesitter-config')            -- syntax hl & language parser
 

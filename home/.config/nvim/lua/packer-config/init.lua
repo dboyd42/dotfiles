69c7260a26b301ce4ce073453187933d764c9352
dboyd42 <bhatmagician@gmail.com>
2023-01-07 22:17:03 -0600

    cleaned up; [+] comment.vim

--- home/.config/nvim/lua/packer-config/init.lua
+++ home/.config/nvim/lua/packer-config/init.lua
@@ -1,53 +1,49 @@
 return require'packer'.startup(function()
-    use 'wbthomason/packer.nvim'           --> packer: KEEP THIS AS LINE #2!
+  use 'wbthomason/packer.nvim'                      --> Packer
 
-    use 'dhruvasagar/vim-table-mode'                       --> Table Generator
-    --> LuaLine: statusline ------------------------------------------------>>>
-    use { 'nvim-lualine/lualine.nvim',                          ---
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true } --- LuaLine
-    }                                                           ---
-    ---> END LuaLine -------------------------------------------------------<<<
-    use 'Mofiqul/dracula.nvim'                              --> Theme: Dracula
-    --> NvimTree: file explorer -------------------------------------------->>>
-    use {
-        'kyazdani42/nvim-tree.lua',                 ---    - NvimTree -
-        requires = {                                ---
-            'kyazdani42/nvim-web-devicons',         --optional, for file icons
-        },                                          ---
-        tag = 'nightly' --optional, updated every week.  (see issue #1193) --
-    }
-    --> END NvimTree -------------------------------------------------------<<<
-    --> Treesitter: highlighting, lang-parser, queries, modules, etc ------->>>
-    use {
-        'nvim-treesitter/nvim-treesitter',          --
-        run = ':TSUpdate'                           --
-    }                                               --> END Treesitter
-    --> Treesitter plugins ------------------------------------------------->>>
-    use 'nvim-treesitter/nvim-treesitter-refactor'  --> Refactor
-    --> END Treesitter -----------------------------------------------------<<<
-    --> LSP plugins -------------------------------------------------------->>>
-    use { 'williamboman/mason.nvim' }                                   --> 1st
-    use 'williamboman/mason-lspconfig.nvim'                             --> 2nd
-    use 'neovim/nvim-lspconfig'    --> 3rd: Collection of configs for built-in LSP client
-    use 'jose-elias-alvarez/null-ls.nvim' --> inject LSP diagnostics, formattings, code actions, and more...
-    use 'L3MON4D3/LuaSnip'         --> Snippets plugin
-    use 'hrsh7th/cmp-nvim-lsp'     --> LSP source for nvim-cmp
-    use 'hrsh7th/nvim-cmp'         --> Autocompletion plugin
-    use 'saadparwaiz1/cmp_luasnip' --> Snippets source for nvim-cmp
-    --> END LSP plugins ----------------------------------------------------<<<
-    use 'onsails/lspkind-nvim' --> vscode-like pictograms for neovim lsp completion items
-    use 'ntpeters/vim-better-whitespace'            --> Whitespace hl & remover
-    use 'rcarriga/nvim-notify'                          --> Notification mgr
-    --> Telescope: fzf ----------------------------------------------------->>>
-    use {                                                   --
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',  -- Telescope
-        requires = { {'nvim-lua/plenary.nvim'} }            --
-    }                                                       --
-    --> END Telescope ------------------------------------------------------<<<
-    use 'nvim-tree/nvim-web-devicons'                       --> Tabline plugin
-    use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'} --> End Tabline
-    -- use 'sunjon/shade.nvim'                  --> Shade: dim inactive panes
-    use 'tpope/vim-repeat'                                 --> Repeat macros
-    use 'tpope/vim-surround'                               --> Modify \B
-    use 'windwp/nvim-autopairs'                         --> Autopairing [{()}]
+  use 'windwp/nvim-autopairs'                       --> Autopairing [{()}]
+  use 'nvim-tree/nvim-web-devicons'                 --> Barbar 1st: Icons
+  use {'romgrk/barbar.nvim',                        --> Barbar 2nd: Tabliner
+    wants = 'nvim-web-devicons'
+  }
+  use {                                             --> Commenter
+  'numToStr/Comment.nvim',
+  config = function()
+    require('Comment').setup()
+  end
+  }
+  use 'Mofiqul/dracula.nvim'                        --> Dracula: Theme
+  use { 'nvim-lualine/lualine.nvim',                --> LuaLine
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  --> LSP plugins ---------------------------------------------------------->>>
+  use { 'williamboman/mason.nvim' }                 --> 1st: LSP Mgr
+  use 'williamboman/mason-lspconfig.nvim'           --> 2nd: LSP Pkg Mgr
+  use 'neovim/nvim-lspconfig'                       --> 3rd: LSP Configs
+  use 'L3MON4D3/LuaSnip'                            --> Snippets plugin
+  use 'hrsh7th/cmp-nvim-lsp'                        --> LSP src for nvim-cmp
+  use 'hrsh7th/nvim-cmp'                            --> Autocompletion
+  use 'saadparwaiz1/cmp_luasnip'                    --> Snippets src for nvim-cmp
+  --> END LSP plugins ------------------------------------------------------<<<
+  use 'rcarriga/nvim-notify'                        --> Notification Mgr
+  use {                                             --> NvimTree: File Explorer
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+        'kyazdani42/nvim-web-devicons', --optional, for file icons
+    },
+    tag = 'nightly' --optional, updated every week.  (see issue #1193)
+  }
+  use 'nvim-treesitter/nvim-treesitter-refactor'    --> Refactor (TreeSitter)
+  use {                                             --> TreeSitter (TS)
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'onsails/lspkind-nvim'                        --> LSPKing: LSP completion icons
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x',  --> Telescope: fzf
+    requires = { 'nvim-lua/plenary.nvim' }
+  }
+  use 'tpope/vim-repeat'                            --> Repeater: macros
+  use 'tpope/vim-surround'                          --> Surroundings
+  use 'dhruvasagar/vim-table-mode'                  --> Table Generator
+  use 'ntpeters/vim-better-whitespace'              --> Whitespace editor
 end)

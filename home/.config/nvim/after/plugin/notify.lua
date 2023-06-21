d0d12f1b90a73cf323bfb0d9532e6c4e0121e722
dboyd42 <bhatmagician@gmail.com>
2023-06-21 08:44:39 -0500

    added <leader>nh to show history

--- home/.config/nvim/after/plugin/notify.lua
+++ home/.config/nvim/after/plugin/notify.lua
@@ -1,13 +1,14 @@
-local notify = require('notify') -- Set as global default notify function
+local notify = require('notify') -- Set as local default notify function
 local map = vim.keymap.set -- Nvim API func (Lua exclusive)
 
 notify.setup({
   background_colour = "#000000",
 })
-require('notify').history() -- List past notifictions
 
 -- Display a sample notification
 map('n', '<leader>ne', ':lua require("notify")("Hi, Mom!")<CR>')
+-- Show notifications history
+map('n', '<leader>nh', ':Telescope notify<CR>')
 -- Close current notification windows
 map('n', '<leader>nc', function() notify.dismiss( { silent = true }) end)
 

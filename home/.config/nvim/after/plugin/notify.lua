1d02f642637ec83b31de285244711c085647d156
dboyd42 <bhatmagician@gmail.com>
2023-06-27 18:03:53 -0500

    added lua opts variable

--- home/.config/nvim/after/plugin/notify.lua
+++ home/.config/nvim/after/plugin/notify.lua
@@ -1,14 +1,15 @@
 local notify = require('notify') -- Set as local default notify function
 local map = vim.keymap.set -- Nvim API func (Lua exclusive)
+local opts = { silent = true }
 
 notify.setup({
   background_colour = "#000000",
 })
 
 -- Display a sample notification
-map('n', '<leader>ne', ':lua require("notify")("Hi, Mom!")<CR>')
+map('n', '<leader>ne', ':lua require("notify")("Hi, Mom!")<CR>', opts)
 -- Show notifications history
-map('n', '<leader>nh', ':Telescope notify<CR>')
+map('n', '<leader>nh', ':Telescope notify<CR>', opts)
 -- Close current notification windows
-map('n', '<leader>nc', function() notify.dismiss( { silent = true }) end)
+map('n', '<leader>nc', function() notify.dismiss( { silent = false }) end, opts)
 

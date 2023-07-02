910e9276b3c7d086ba6232132f853b578ff0f14a
dboyd42 <bhatmagician@gmail.com>
2023-07-02 11:42:12 -0500

    MoveBufferNext/Previous A-J/K (prevent collision w/ macOS Amethyst)

--- home/.config/nvim/after/plugin/barbar.lua
+++ home/.config/nvim/after/plugin/barbar.lua
@@ -45,8 +45,10 @@ map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)     -- 'n', C-h/l
 map('n', '<A-h>', '<Cmd>BufferPrevious<CR>', opts) -- Similar mapping:
 map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)     -- 'n', C-h/l
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts) -- conflicts w/ macOS
+-- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)    -- Amethyst tiling mgr
+map('n', '<A-j>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A-k>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
 map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
 map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)

eb248ceee69a5aecd8059e31cd5b45f2ecdb75d7
dboyd42 <bhatmagician@gmail.com>
2023-09-14 00:29:19 -0500

    Update auto-install via bun

--- home/.config/nvim/lua/plugins/markdown-preview.lua
+++ home/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,8 @@
 return {
   "iamcco/markdown-preview.nvim",
 
-  build = "cd app && bun install", -- This installs the plugin's dependencies
+  -- build = "cd app && bun install", -- This installs the plugin's dependencies
+  build = "bun install $HOME/.local/share/nvim/lazy/markdown-preview.nvim/app/",
   setup = function()
       vim.g.mkdp_auto_start = 0 -- !Auto open .md files in preview mode
   end,
@@ -12,15 +13,3 @@ return {
     { "<leader>mdp", "<Cmd>MarkdownPreviewToggle<CR>", desc = "Prev Buffer" },
   },
 }
-
---[[ Installation:
-
-ArchLinux:
-  1. May still need to run `:call mkdp#util#install()`
-  *Note: testing is required: macOS works with current configs
-
-macOS:
-  1. Install the plugin normally
-  2. `bun install $HOME/.local/share/nvim/lazy/markdown-preview`
-
---]]

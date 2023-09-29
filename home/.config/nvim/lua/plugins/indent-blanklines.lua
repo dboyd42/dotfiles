44da0dce6043db97343818209667ee0d2280b093
dboyd42 <bhatmagician@gmail.com>
2023-09-29 14:32:47 -0500

    updated to version 3 && rm prepending dots

--- home/.config/nvim/lua/plugins/indent-blanklines.lua
+++ home/.config/nvim/lua/plugins/indent-blanklines.lua
@@ -3,12 +3,12 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
         vim.opt.list = true
-        vim.opt.listchars:append("space:⋅")
+        -- vim.opt.listchars:append("space:⋅")
         vim.opt.listchars:append("eol:↴")
 
-      require("indent_blankline").setup {
+      require("ibl").setup {
         show_end_of_line = true,
-        space_char_blankline = " ",
+        -- space_char_blankline = " ",
         show_current_context = true,
         show_current_context_start = true,
       }

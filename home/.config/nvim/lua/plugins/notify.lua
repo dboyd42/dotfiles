678d3b243607f09df6d2fdb61866ebc18622a632
dboyd42 <bhatmagician@gmail.com>
2024-02-15 14:50:32 -0600

    mapped 'i,n' <localleader>Space fns: notify.dismiss && vim.cmd('nohl')

--- home/.config/nvim/lua/plugins/notify.lua
+++ home/.config/nvim/lua/plugins/notify.lua
@@ -8,14 +8,22 @@ return {
       vim.notify = require("notify")
 
       notify.setup({
-        background_colour = "#000000",
+        background_colour = "#000000",  -- Transparency
       })
 
+      --[[ Functions --------------------------------------------------------]]
+      -- Dismiss notifications and clear search highlighting
+      local function dismissAndClearSearch()
+        notify.dismiss({ silent = false }) -- Dismiss notifications
+        vim.cmd('nohl') -- Clear search highlighting
+      end
+
+      --[[ Keymaps ----------------------------------------------------------]]
       map('n', '<leader>ne', ':lua require("notify")("Hi, Mom!")<CR>', { silent = true })
       map('n', '<leader>nh', ':Telescope notify<CR>', { silent = true })
       map('n', '<leader>nc', function() notify.dismiss({ silent = false }) end, { silent = true })
-      map('i', '<localleader> ', function() notify.dismiss({ silent = false }) end, { silent = true })
-      map('i', '<localleader>nc', function() notify.dismiss({ silent = false }) end, { silent = true })
+      map('n', '<localleader> ', dismissAndClearSearch, { silent = true })
+      map('i', '<localleader> ', dismissAndClearSearch, { silent = true })
     end,
   },
 }

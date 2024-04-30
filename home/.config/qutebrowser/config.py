cf92d80609845f3ecf5815ae62bef6d70fb8acf2
dboyd42 <bhatmagician@gmail.com>
2024-04-30 15:41:07 -0500

    WIP configuring...

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -1,10 +1,11 @@
 # https://www.qutebrowser.org/doc/help/settings.html#content.local_content_can_access_file_urls
 
+
 # -----------------------------------------------------------------------------
 # Theme
 # -----------------------------------------------------------------------------
 # Setting dark mode
-config.set("colors.webpage.darkmode.enabled", True)
+config.set('colors.webpage.darkmode.enabled', True)
 
 # Dracula
 # -------
@@ -17,6 +18,7 @@ dracula.draw.blood(c, {
         'horizontal': 8
     }
 })
+
 
 # -----------------------------------------------------------------------------
 # Cookies
@@ -34,6 +36,7 @@ config.set('content.headers.accept_language', 'en-US,en;q=0.9')
 
 # User agent (default)
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {qt_key}/{qt_version} {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}')
+
 
 # -----------------------------------------------------------------------------
 # JavaScript
@@ -56,31 +59,59 @@ config.set('content.local_content_can_access_file_urls', True)
 # Allow locally loaded documents to access remote URLs (default)
 config.set('content.local_content_can_access_remote_urls', False)
 
+
 # -----------------------------------------------------------------------------
 # Keybindings
 # -----------------------------------------------------------------------------
+
+# ------------------------
 # Bindings for normal mode
-config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
-config.bind('xb', 'config-cycle statusbar.show always never')
-config.bind('xt', 'config-cycle tabs.show always never')
-config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
-config.bind('\sc', 'config-source ~/.config/qutebrowser/config.py')
+# ------------------------
+
+# Bookmarks and Quickmarks
+# ------------------------
+# Open a captive portal page
+config.unbind('ss')  # alias `:set`
+config.bind('ssl', 'open -t http://neverssl.com')
+
+# General Bindings
+# ----------------
+# Escape Passthrough Mode
 config.bind('<Ctrl-g>', 'leave-mode', mode='passthrough')
 
+# Source Qutebrowser's Configuration File
+config.bind('\sc', 'config-source ~/.config/qutebrowser/config.py')
+
+# Statusbar/Titlebar Show/Hide
+config.bind('xb', 'config-cycle statusbar.show always never')
+config.bind('xx', 'config-cycle statusbar.show always never; config-cycle tabs.show always never')
+config.bind('xt', 'config-cycle tabs.show always never')
+
+# Videos
+# ------
+# Watch Videos Without Ads via MPV
+config.bind('M', 'hint links spawn mpv {hint-url}')
+# Download Videos
+config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
+
+# ------------------------
+# Bindings for Insert Mode
+# ------------------------
+
 # Readline Insert Mode for Emacs-like Bindings
-config.bind("<Ctrl-h>", "fake-key <Backspace>", "insert")
-config.bind("<Ctrl-e>", "fake-key <Home>", "insert")
-config.bind("<Ctrl-b>", "fake-key <End>", "insert")
-config.bind("<Mod1-b>", "fake-key <Left>", "insert")
-config.bind("<Ctrl-f>", "fake-key <Ctrl-Left>", "insert")
-config.bind("<Mod1-f>", "fake-key <Right>", "insert")
-config.bind("<Ctrl-p>", "fake-key <Ctrl-Right>", "insert")
-config.bind("<Ctrl-n>", "fake-key <Up>", "insert")
-config.bind("<Ctrl-d>", "fake-key <Down>", "insert")
-config.bind("<Mod1-d>", "fake-key <Ctrl-Delete>", "insert")
-config.bind("<Ctrl-d>", "fake-key <Delete>", "insert")
-config.bind("<Ctrl-w>", "fake-key <Ctrl-Backspace>", "insert")
-config.bind("<Ctrl-u>", "fake-key <Shift-Home>", "insert")
-config.bind("<Ctrl-k>", "fake-key <Shift-End>", "insert")
-config.bind("<Ctrl-x><Ctrl-e>", "fake-key <open-editor>", "insert")
+# --------------------------------------------
+config.bind('<Ctrl-h>', 'fake-key <Backspace>', 'insert')
+config.bind('<Ctrl-e>', 'fake-key <Home>', 'insert')
+config.bind('<Ctrl-b>', 'fake-key <End>', 'insert')
+config.bind('<Mod1-b>', 'fake-key <Left>', 'insert')
+config.bind('<Ctrl-f>', 'fake-key <Ctrl-Left>', 'insert')
+config.bind('<Mod1-f>', 'fake-key <Right>', 'insert')
+config.bind('<Ctrl-p>', 'fake-key <Ctrl-Right>', 'insert')
+config.bind('<Ctrl-n>', 'fake-key <Up>', 'insert')
+config.bind('<Ctrl-d>', 'fake-key <Down>', 'insert')
+config.bind('<Mod1-d>', 'fake-key <Ctrl-Delete>', 'insert')
+config.bind('<Ctrl-d>', 'fake-key <Delete>', 'insert')
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-Backspace>', 'insert')
+config.bind('<Ctrl-u>', 'fake-key <Shift-Home>', 'insert')
+config.bind('<Ctrl-k>', 'fake-key <Shift-End>', 'insert')
+config.bind('<Ctrl-x><Ctrl-e>', 'fake-key <open-editor>', 'insert')
